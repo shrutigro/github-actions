0588ce8c1f79e07dd23133cb073c87608846651b
shrutigro <141320344+shrutigro@users.noreply.github.com>
2025-08-19 00:15:09 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -21,4 +21,7 @@ provider "azurerm" {
   }
 }
 subscription_id = "381b371f-c06d-4487-a677-8d1e6f61613f"
+client_id       = "03ebe487-6f3a-438c-bd4c-a4ac21d156ae"
+client_secret   = "SkK8Q~v9NSdoHEmR3TvmsOFA~NJ4luL5nw0hUczF"
+tenant_id       = "26e60ec1-7b36-467d-b345-01978ec43d5f"
 }
