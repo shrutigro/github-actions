2ca020dd2fe52971e9cede963e835cb6f1922a06
shrutigro <141320344+shrutigro@users.noreply.github.com>
2025-08-19 00:06:35 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -4,11 +4,7 @@ terraform {
     storage_account_name = "12345678910"       # Can be passed via `-backend-config=`"storage_account_name=<storage account name>"` in the `init` command.
     container_name       = "tfstate-container" # Can be passed via `-backend-config=`"container_name=<container name>"` in the `init` command.
     key                  = "terraform.tfstate" # Can be passed via `-backend-config=`"key=<blob key name>"` in the `init` command.
-    subscription_id       = ${{ secrets.AZURE_SUBSCRIPTION_ID }}
-    tenant_id             =  ${{ secrets.AZURE_TENANT_ID }}
-    client_id             = ${{ secrets.AZURE_CLIENT_ID }}
-    client_secret         = ${{ secrets.AZURE_CLIENT_SECRET }}
-
+  
 
         
   }
