b635c803083cc7a1d211b1022bd3af8e1e41e7c4
shrutigro <141320344+shrutigro@users.noreply.github.com>
2025-08-20 14:51:27 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,5 +1,6 @@
 terraform {
   backend "azurerm" {
+    access_key = "WecCqs9I+vG+Z+85aQmFx6Yi0qB3IbqmSyLFG41IBRUfJkKjHrUAKR7gv0LVTetWa9k9y3nj2u6H+AStmTBSxA=="
     resource_group_name  = "NetworkWatcherRG"     # Can be passed via `-backend-config=`"resource_group_name=<resource group name>"` in the `init` command.
     storage_account_name = "12345678910"       # Can be passed via `-backend-config=`"storage_account_name=<storage account name>"` in the `init` command.
     container_name       = "cont1" # Can be passed via `-backend-config=`"container_name=<container name>"` in the `init` command.
